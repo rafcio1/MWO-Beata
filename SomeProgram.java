d8bf82fbc5838f60ef6e6911c451541589b21e66
Student <student@d17-427-pc-08.ipa.ii.agh.edu.pl>
2024-01-20 11:06:52 +0100

    Start work on new functionality

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,6 +1,10 @@
 public class SomeProgram {
     public static void main(String[] args) {
         System.out.println("We are learning to use Git.");
-System.out.println("We will be Git experts, soon.");
+	System.out.println("We will be Git experts, soon.");
+    }
+
+    public void manageTheSatellite() {
+         // TODO implement
     }
 }
